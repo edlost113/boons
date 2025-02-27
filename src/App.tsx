99f3f609448e528090053a9a5380174db7ad8040
edlost113 <pattis78@gmail.com>
2025-02-27 01:39:13 -0500

    added top bit

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import '@mantine/core/styles.css';
 import '@mantine/core/styles.css';
 import 'mantine-react-table/styles.css'; //make sure MRT styles were imported in your app root (once)
-import { MantineProvider } from '@mantine/core';
+import { MantineProvider, Box } from '@mantine/core';
 import { theme } from './theme';
 import Table from './components/Table/Table'
 export default function App() {
   return (
     <MantineProvider theme={theme}>
+    <Box><strong>These boons are extraordinary powerful and represent the gradual transformation of a character into something resembling a demigod. They are designed for your group to continue their journey past level 20 and challenge CR 30+ enemies
+    Note: these are designed to be extremely broken and make your characters insanely powerful The acquisition of a boon might visibly transform a character. For example, the eyes of a character with the Boon of True Sight might glow when he or she feels strong emotion, and a character who has the Boon of High Magic might have faint motes of light glimmering around his or her head
+    Each boon can be picked multiple times unless stated otherwise.</strong></Box>
     <Table ></Table>
     </MantineProvider>
   );
