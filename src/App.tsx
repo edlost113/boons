3852d8c2e09d4ad023cf999bf1050040e42de2ee
edward <pattis78@gmail.com>
2025-02-27 09:51:18 -0500

    initial Mobile support

--- src/App.tsx
+++ src/App.tsx
@@ -4,13 +4,30 @@ import 'mantine-react-table/styles.css'; //make sure MRT styles were imported in
 import { MantineProvider, Box } from '@mantine/core';
 import { theme } from './theme';
 import Table from './components/Table/Table'
+import Mobile from './components/Mobile/Mobile'
+import { useEffect, useState } from 'react';
 export default function App() {
+
+  const [isMobile, setIsMobile] = useState(false);
+  useEffect(() => {
+    const mediaQuery = window.matchMedia('(max-width: 767px)');
+    let urlParams = new URLSearchParams(window.location.search);
+    let isMobile = mediaQuery.matches || ('mobile' === urlParams.get("device"));
+    setIsMobile(isMobile);
+
+    const handleResize = () => setIsMobile(mediaQuery.matches);
+    mediaQuery.addEventListener('change', handleResize);
+    
+    return () => mediaQuery.removeEventListener('change', handleResize);
+  }, []);
+
+
   return (
     <MantineProvider theme={theme}>
     <Box><strong>These boons are extraordinary powerful and represent the gradual transformation of a character into something resembling a demigod. They are designed for your group to continue their journey past level 20 and challenge CR 30+ enemies
     Note: these are designed to be extremely broken and make your characters insanely powerful The acquisition of a boon might visibly transform a character. For example, the eyes of a character with the Boon of True Sight might glow when he or she feels strong emotion, and a character who has the Boon of High Magic might have faint motes of light glimmering around his or her head
     Each boon can be picked multiple times unless stated otherwise.</strong></Box>
-    <Table ></Table>
+    {!isMobile ? <Table /> : <Mobile  />}
     </MantineProvider>
   );
 }
---
+++ src/components/Mobile/Mobile.tsx
@@ -0,0 +1,75 @@
+import {
+    MantineReactTable,
+    useMantineReactTable,
+    type MRT_ColumnDef,
+  } from 'mantine-react-table';
+  import { data, type Boon } from '../../data';
+  import { Box } from '@mantine/core';
+  import { useMemo } from 'react';
+
+const Mobile = () => {
+    const columns = useMemo<MRT_ColumnDef<Boon>[]>(
+        () => [
+          {
+            header: 'Boon Name',
+            size: 250,
+            accessorKey: 'name',
+          },
+          {
+            header: 'Boon Description',
+            accessorKey: 'desc',
+            grow: true,
+          },
+          {
+            header: 'Title Gained',
+            accessorKey: 'title',
+          },
+          {
+            header: 'Requirement',
+            accessorKey: 'pre',
+            size: 250,
+            GroupedCell: ({ cell, row }) => (
+              <Box style={{ color: 'skyblue' }}>
+                <strong>{cell.getValue<string>()}</strong> ({row.subRows?.length})
+              </Box>
+            ),
+          },
+          {
+            header: 'Boon Points',
+            accessorKey: 'lvl',
+            enableGrouping: true, 
+            GroupedCell: ({ cell, row }) => (
+                <Box style={{ color: 'skyblue' }}>
+                  <strong>{cell.getValue<string>()} Boon Points </strong> ({row.subRows?.length})
+                </Box>
+              ),
+          }
+        ],[]);
+
+
+    const table = useMantineReactTable({
+        columns,
+        data,
+        enableColumnResizing: true,
+        enableGrouping: true,
+        enableGlobalFilter: false,
+        enableStickyHeader: true,
+        enableStickyFooter: true,
+        enableBottomToolbar: false,
+        enableDensityToggle: false,
+        layoutMode: 'grid-no-grow',
+        initialState: {
+          density: 'md',
+          expanded: true,
+          grouping: ['lvl', 'pre'],
+          columnVisibility: { title: false, pre: false },
+          pagination: { pageIndex: 0, pageSize: 200 },
+          sorting: [{ id: 'lvl', desc: false }],
+        },
+        mantineToolbarAlertBannerBadgeProps: { color: 'blue', variant: 'outline' },
+        mantineTableContainerProps: { style: { maxHeight: 700 } },
+      });
+    return <MantineReactTable table={table} />;
+};
+
+export default Mobile;
