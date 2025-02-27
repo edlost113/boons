a95aadc0a7086cbda9cd5550f97d6b881442302c
edlost113 <pattis78@gmail.com>
2025-02-27 01:45:36 -0500

    color scheme stuff

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ import { theme } from './theme';
 import Table from './components/Table/Table'
 export default function App() {
   return (
-    <MantineProvider theme={theme}>
+    <MantineProvider defaultColorScheme="auto" theme={theme}>
     <Box><strong>These boons are extraordinary powerful and represent the gradual transformation of a character into something resembling a demigod. They are designed for your group to continue their journey past level 20 and challenge CR 30+ enemies
     Note: these are designed to be extremely broken and make your characters insanely powerful The acquisition of a boon might visibly transform a character. For example, the eyes of a character with the Boon of True Sight might glow when he or she feels strong emotion, and a character who has the Boon of High Magic might have faint motes of light glimmering around his or her head
     Each boon can be picked multiple times unless stated otherwise.</strong></Box>
