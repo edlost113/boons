b0e668620a0d0e846e4c9f366c2033a330a9b813
edward <pattis78@gmail.com>
2025-02-27 11:20:29 -0500

    More Mobile

--- src/components/Mobile/Mobile.tsx
+++ src/components/Mobile/Mobile.tsx
@@ -24,6 +24,8 @@ const Mobile = () => {
               <Box style={{ color: 'skyblue' }}>{cell.getValue<string>()}</Box>
               <Box>{cell.row.original.desc}</Box>
               <Box><strong>Title:</strong> {cell.row.original.title}</Box>
+              <Box><strong>Prerequisite:</strong> {cell.row.original.pre}</Box>
+              <Box><strong>Boon Points:</strong> {cell.row.original.lvl}</Box>
             </Stack>
             </>
             ),
@@ -64,8 +66,8 @@ const Mobile = () => {
         initialState: {
           density: 'md',
           expanded: true,
-          grouping: ['lvl', 'pre'],
-          columnVisibility: { title: false, },
+          //grouping: ['lvl', 'pre'],
+          columnVisibility: { title: false, lvl: false, pre: false },
           pagination: { pageIndex: 0, pageSize: 200 },
           sorting: [{ id: 'lvl', desc: false }],
         },
